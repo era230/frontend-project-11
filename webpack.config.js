817f18e8a28a05b1ed083b45ccde2c57c723ca4c
Elvira Aitkulova <aitkulovael@gmail.com>
2022-11-29 20:35:48 +0300

    add build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -19,8 +17,6 @@ const config = {
       template: 'index.html',
     }),
 
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   mode: process.env.NODE_ENV || 'development',
   module: {
@@ -30,17 +26,7 @@ const config = {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
       },
-      {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
-      },
     ],
-    // Add your rules for custom modules here
-    // Learn more about loaders from https://webpack.js.org/loaders/
 
   },
 };
